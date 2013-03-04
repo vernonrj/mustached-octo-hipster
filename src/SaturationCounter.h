ce4c2700af6f1e83ac3cc2b58a16f865e41d7d39
apropos <ttricker@pdx.edu>
2013-03-04 13:34:17 -0800

    index on master: 02ecc9f added SaturationCounter Class

---
+++ src/SaturationCounter.h
@@ -0,0 +1,53 @@
+#include<stdint.h>
+
+// private classes
+class SaturationCounter
+{
+public:
+    SaturationCounter(uint8_t numofbits, uint8_t initialValue = 0)
+    {
+        // Sanity Check parameters
+        if((numofbits >= 8) || (numofbits < 1))
+            numofbits = 1;
+
+        if(initialValue > (1 << numofbits))
+            m_CounterValue = 1;
+
+        m_SizeInBits = numofbits;
+ 
+    }
+    /**
+    * Ripple Counter Implementation
+    */
+    SaturationCounter& operator++()
+    {
+        if(m_CounterValue < (1 << m_SizeInBits)) 
+            ++m_CounterValue;
+        return *this;
+    }
+
+    /**
+    * Decrement Counter until it saturates to a minimum
+    */
+    SaturationCounter& operator--()
+    {
+        if(m_CounterValue != 0)
+            --m_CounterValue;
+        return *this;
+    }
+
+    uint8_t GetMemoryUsage()
+    {
+        return m_SizeInBits;
+    }
+
+    uint8_t GetCounterValue()
+    {
+        return m_CounterValue;
+    }
+
+private:
+    static const uint64_t COUNTERMAX = 255;
+    uint8_t m_CounterValue;
+    uint8_t m_SizeInBits;
+};
